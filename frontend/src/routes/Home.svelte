4e3f3e2e979f83ce7f72321c4c2e234b80b65feb
KDS <kddddds@gmail.com>
2023-01-26 09:43:12 +0000

    질문 답변 api frontend 만듬

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,5 +1,7 @@
 <script>
     import fastapi from "../lib/api"
+    import { link } from 'svelte-spa-router'
+
     
     let question_list = []
     
@@ -14,6 +16,8 @@
 
 <ul>
     {#each question_list as question}
-        <li>{question.subject}</li>
+        <li>
+            <a use:link href="/detail/{question.id}">{question.subject}</a>
+        </li>
     {/each}
 </ul>
