f8d8d87bf23ac47502168b8ea7aea1256f7bfdf5
KDS <kddddds@gmail.com>
2023-01-26 02:28:27 +0000

    pydantic 스키마 작성

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from database import SessionLocal
+from database import get_db
+from domain.question import question_schema
 from models import Question
 
 
 router = APIRouter(prefix="/api/question")
 
-@router.get("/list")
+@router.get("/list", response_model = list[question_schema.Question])
 def question_list(db:Session=Depends(get_db)):
-    db = SessionLocal()
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close()
     return _question_list
